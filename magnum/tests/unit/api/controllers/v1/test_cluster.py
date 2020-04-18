c79cae1bdfdb1db92db920f0fb12ef9780c13c71
Sean McGinnis <sean.mcginnis@gmail.com>
2020-04-18 11:54:00 -0500

    Use unittest.mock instead of third party mock
    Now that we no longer support py27, we can use the standard library
    unittest.mock module instead of the third party mock lib.
    
    Change-Id: I6245a207ec65df0ef11a798b4cffe3157abccb73
    Signed-off-by: Sean McGinnis <sean.mcginnis@gmail.com>
    Co-Authored-By: Spyros Trigazis <spyridon.trigazis@cern.ch>

--- magnum/tests/unit/api/controllers/v1/test_cluster.py
+++ magnum/tests/unit/api/controllers/v1/test_cluster.py
@@ -11,8 +11,8 @@
 #    limitations under the License.
 
 import datetime
+from unittest import mock
 
-import mock
 from oslo_config import cfg
 from oslo_serialization import jsonutils
 from oslo_utils import timeutils
