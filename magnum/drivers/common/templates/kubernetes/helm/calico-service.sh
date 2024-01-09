9acfb30d9883ef7fc87b2b49bc1e0a5c77bdc219
Jakub Darmach <jakub@stackhpc.com>
2024-01-09 13:50:54 +0100

    Tigera Operator Helm install moved outside of metachart

--- magnum/drivers/common/templates/kubernetes/helm/calico-service.sh
+++ magnum/drivers/common/templates/kubernetes/helm/calico-service.sh
@@ -7,18 +7,35 @@ CHART_NAME="tigera-operator"
 if [ "$NETWORK_DRIVER" = "calico" ]; then
     echo "Writing ${CHART_NAME} config"
 
-    HELM_CHART_DIR="/srv/magnum/kubernetes/helm/magnum"
+    # HELM_CHART_DIR="/srv/magnum/kubernetes/helm/magnum"
+    HELM_CHART_DIR="/srv/magnum/kubernetes/helm/calico"
     mkdir -p ${HELM_CHART_DIR}
 
-    cat << EOF >> ${HELM_CHART_DIR}/requirements.yaml
-- name: ${CHART_NAME}
-  version: ${CALICO_TAG}
-  repository: https://projectcalico.docs.tigera.io/charts
-EOF
+#     cat << EOF >> ${HELM_CHART_DIR}/requirements.yaml
+# - name: ${CHART_NAME}
+#   version: ${CALICO_TAG}
+#   repository: https://projectcalico.docs.tigera.io/charts
+# EOF
     cat << EOF >> ${HELM_CHART_DIR}/values.yaml
 tigera-operator:
   installation:
     flexVolumePath: /opt/kubernetes/kubelet-plugins/volume/exec/
   flexVolumePluginDir: /var/lib/kubelet/volumeplugins
 EOF
+  helm_prepare_cmd="helm repo add projectcalico https://docs.tigera.io/calico/charts"
+  helm_install_cmd="helm install calico projectcalico/tigera-operator --version ${CALICO_TAG} -f values.yaml --namespace tigera-operator"
+  helm_history_cmd="helm history calico --namespace tigera-operator"
+
+  if [[ -d "${HELM_CHART_DIR}" ]]; then
+      pushd ${HELM_CHART_DIR}
+      $helm_prepare_cmd
+      i=0
+      until ($helm_history_cmd | grep calico | grep deployed) || (helm dep update && $helm_install_cmd); do
+          i=$((i + 1))
+          [ $i -lt 60 ] || break;
+          sleep 5
+      done
+      popd
+  fi
+
 fi
