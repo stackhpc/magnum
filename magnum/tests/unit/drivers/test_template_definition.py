15aee8b5cec919c6a130f5903044c5e29949a022
Seunghun Lee <seunghun@stackhpc.com>
2024-07-03 13:51:37 +0100

    Fix flake8: Too many blank lines

--- magnum/tests/unit/drivers/test_template_definition.py
+++ magnum/tests/unit/drivers/test_template_definition.py
@@ -1116,7 +1116,6 @@ class FCOSK8sTemplateDefinitionTestCase(BaseK8sTemplateDefinitionTestCase):
         extra_subnet = mock_cluster.labels.get('extra_subnet')
         extra_security_group = mock_cluster.labels.get('extra_security_group')
 
-
         octavia_provider = mock_cluster.labels.get('octavia_provider')
         octavia_lb_algorithm = mock_cluster.labels.get('octavia_lb_algorithm')
         octavia_lb_healthcheck = mock_cluster.labels.get(
